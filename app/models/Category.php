fc0cb877907dae20b7331471219a6bb702d52a53
Thomas Babu <tommy.developer@gmail.com>
2014-10-22 15:55:54 +0800

    created products model and update category model with other table relationship

--- app/models/Category.php
+++ app/models/Category.php
@@ -5,4 +5,9 @@ class Category extends Eloquent {
     protected $fillable = array('name');
 
     public static $rules = array('name' => 'required|min:3');
+
+    public function products()
+    {
+        return $this->hasMany('Product');
+    }
 }
